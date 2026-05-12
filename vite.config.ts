16284142e74f5a4769c1e1a729c69f146df0bb5a
Slava Mryasov <slavadevbox1@gmail.com>
2026-05-12 18:26:46 +0700

    feat: add base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: process.env.GITHUB_PAGES ? '/upYourEnglish/' : '/',
   plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
